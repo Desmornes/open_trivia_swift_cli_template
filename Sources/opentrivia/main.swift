125ff85745aef1ec2a441f32ddb65afe18c3223c
Nephtalie Desmornes <137033061+Desmornes@users.noreply.github.com>
2024-12-17 22:54:24 +0000

    ajout API

--- Sources/opentrivia/main.swift
+++ Sources/opentrivia/main.swift
@@ -2,15 +2,39 @@
 // https://docs.swift.org/swift-book
 
 import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
 import Dispatch
 
 print("Open Trivia")
 
+let url = URL(string: "https://opentdb.com/api.php?amount=10")!
+
 // NB: Replace the `DispatchQueue.main.asyncAfter` with your code.
 // Once the program is done, call `exit(EXIT_SUCCESS)` to exit the program.
-DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-    print("I'm done!")
-    exit(EXIT_SUCCESS)
+
+func fetchTriviaQuestions() {
+    let task = URLSession.shared.dataTask(with: url) { data, response, error in
+        if let error = error {
+            print("Erreur lors de l'appel API : \(error.localizedDescription)")
+            exit(EXIT_FAILURE)
+        }
+
+        if let data = data {
+            // Afficher les données brutes pour vérifier
+            print("Données reçues :")
+            print(String(data: data, encoding: .utf8) ?? "Impossible d'afficher les données")
+            
+            // Appel terminé
+            exit(EXIT_SUCCESS)
+        }
+    }
+    task.resume() // On lance la requête
+}
+
+DispatchQueue.main.async {
+    fetchTriviaQuestions()
 }
 
 dispatchMain()
